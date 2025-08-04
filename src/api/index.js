caaeabae8e2494515a58d532ac9c1b344b285d1f
abuhasanmahmud <80718125+abuhasanmahmud@users.noreply.github.com>
2025-08-04 16:14:09 +0600

    update..

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 
 import dotenv from "dotenv/config";
-import authRoutes from "./routes/authRoutes.js";
-import bookRoutes from "./routes/bookRoutes.js";
-import { connectDB } from "./lib/db.js";
+import authRoutes from "../routes/authRoutes.js";
+import bookRoutes from "../routes/bookRoutes.js";
+import { connectDB } from "../lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
