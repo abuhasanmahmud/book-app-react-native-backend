7512871f999d025c58b9b631c652442042ae5b40
abuhasanmahmud <80718125+abuhasanmahmud@users.noreply.github.com>
2025-08-04 16:00:26 +0600

    vercel.json

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
+app.get("/", (req, res) => {
+  res.send("App works properly!");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
