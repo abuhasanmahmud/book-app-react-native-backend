0b7c2ad465dcc5bf21ed1055dcab4221fa296dbc
abuhasanmahmud <80718125+abuhasanmahmud@users.noreply.github.com>
2025-08-07 10:13:54 +0600

    update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -94,4 +94,15 @@ router.post("/login", async (req, res) => {
   }
 });
 
+//get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find().select("-password"); // exclude passwords
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 export default router;
